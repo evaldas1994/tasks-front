c3e7382b16a660d719627031ae9aba530a5a34de
evald <evaldas.tuleikis@gmail.com>
2025-09-14 11:02:11 +0300

    Atskiriami manifestai

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 // import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import manifestIf from './public/manifest-if.json'
+import manifestUlala from './public/manifest-ulala.json'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -12,8 +15,8 @@ export default defineConfig({
     // vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate', // automatinis SW atnaujinimas
-      includeAssets: ['favicon.svg', 'robots.txt', 'pwa-192x192.png', 'pwa-512x512.png'],
-      manifest: 'public/manifest-if.json'
+      includeAssets: ['favicon.svg', 'robots.txt', 'if/pwa-192x192.png', 'if/pwa-512x512.png'],
+      manifest: manifestIf
     }),
   ],
   resolve: {
