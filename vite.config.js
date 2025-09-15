e4d1c2f2d6418339520ac04beab99ffc6cd06118
evald <evaldas.tuleikis@gmail.com>
2025-09-15 18:43:01 +0300

    Revertinau po nesėkmingo manifest skaidymo

--- vite.config.js
+++ vite.config.js
@@ -1,49 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    // vueDevTools(),
-    VitePWA({
-      registerType: 'autoUpdate', // automatinis SW atnaujinimas
-      includeAssets: ['favicon.svg', 'robots.txt', 'pwa-192x192.png', 'pwa-512x512.png'],
-      manifest: {
-        name: 'Ulala',
-        short_name: 'Ulala',
-        description: 'Ulala kolekcija',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'fullscreen',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  // įkrauna env failus pagal aplinką
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return {
+    plugins: [
+      vue(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'robots.txt', 'pwa-192x192.png', 'pwa-512x512.png'],
+        manifest: {
+          name: env.VITE_NAME,
+          short_name: env.VITE_SHORT_NAME,
+          description: env.VITE_APP_DESCRIPTION,
+          theme_color: env.VITE_THEME_COLOR,
+          background_color: env.VITE_BACKGROUND_COLOR,
+          display: env.VITE_DISPLAY,
+          orientation: 'portrait',
+          start_url: env.VITE_START_URL,
+          icons: [
+            { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+            { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+          ]
+        }
+      }),
+    ],
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5175,
+    server: { host: '0.0.0.0', port: 5175 },
   }
 })
