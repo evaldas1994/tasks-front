c02fe6d1b8ee563be2f5df07c81c956bb8aaf4a4
evald <evaldas.tuleikis@gmail.com>
2025-09-16 18:03:49 +0300

    Revertinau po nesėkmingo manifest skaidymo

--- vite.config.js
+++ vite.config.js
@@ -27,9 +27,6 @@ export default defineConfig(({ mode }) => {
             { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
             { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
           ]
-        },
-        build: {
-          outDir: 'public_html'
         }
       }),
     ],
@@ -37,6 +34,9 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, './src')
       }
+    },
+    build: {
+      outDir: 'public_html'
     }
     // resolve: {
     //   alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
