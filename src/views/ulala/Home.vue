442022c23521dfe0ae6d5a725fd183b71ca77921
evald <evaldas.tuleikis@gmail.com>
2025-09-19 19:27:48 +0300

    Blur

--- src/views/ulala/Home.vue
+++ src/views/ulala/Home.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="d-flex flex-column vh-100 mx-2 gap-2">
     Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
+    Jūratėlei
   </div>
 </template>
 
