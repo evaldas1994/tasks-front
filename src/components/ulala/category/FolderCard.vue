1022852226c2f6a26c4a85ead0e769668a62feb0
evald <evaldas.tuleikis@gmail.com>
2025-09-19 20:27:58 +0300

    Folderio kortelės komponentas

--- src/components/ulala/category/FolderCard.vue
+++ src/components/ulala/category/FolderCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="p-1 main-color">
+  <div class="p-1">
     <div class="rounded">
       <img
         src="https://picsum.photos/300/300"
         alt=""
         class="w-100 rounded-top"
-        style="height: 50px; object-fit: cover;"
+        style="height: 80px; object-fit: cover;"
       >
       <div class="p-1 border rounded-bottom bg-white bg-opacity-50">
-        <div class="font-size-rem-0-6 ">Pavadinimas kartai</div>
+        <div class="font-size-rem-0-7 main-color-darken-3">Pavadinimas kartai</div>
         <div class="d-flex justify-content-between">
-          <div class="font-size-rem-0-6"><i class="bi bi-archive"></i> 255</div>
-          <div class="font-size-rem-0-6"><i class="bi bi-disc"></i> 125GB</div>
+          <div class="font-size-rem-0-6 main-color-darken"><i class="bi bi-archive"></i> 255</div>
+          <div class="font-size-rem-0-6 main-color-darken"><i class="bi bi-disc"></i> 125GB</div>
         </div>
       </div>
 
--- src/views/ulala/Home.vue
+++ src/views/ulala/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="row m-2">
     <div
-      v-for="(folder, key) in [1,2,3,4,5,6]"
+      v-for="(folder, key) in [1,2,3,4,5,6,7]"
       :key="key"
-      class="col-3 g-0">
+      class="col-4 g-0">
       <folder-card/>
     </div>
   </div>
