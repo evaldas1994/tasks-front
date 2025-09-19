e070a0f66100817acd95d870fd53f70ab87c1c81
evald <evaldas.tuleikis@gmail.com>
2025-09-19 20:20:34 +0300

    Folderio kortelės komponentas

---
+++ src/components/ulala/category/FolderCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-1 main-color">
+    <div class="rounded">
+      <img
+        src="https://picsum.photos/300/300"
+        alt=""
+        class="w-100 rounded-top"
+        style="height: 50px; object-fit: cover;"
+      >
+      <div class="p-1 border rounded-bottom bg-white bg-opacity-50">
+        <div class="font-size-rem-0-6 ">Pavadinimas kartai</div>
+        <div class="d-flex justify-content-between">
+          <div class="font-size-rem-0-6"><i class="bi bi-archive"></i> 255</div>
+          <div class="font-size-rem-0-6"><i class="bi bi-disc"></i> 125GB</div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/ulala/Home.vue
+++ src/views/ulala/Home.vue
@@ -1,50 +1,17 @@
 <template>
-  <div class="d-flex flex-column vh-100 mx-2 gap-2">
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
-    Jūratėlei
+  <div class="row m-2">
+    <div
+      v-for="(folder, key) in [1,2,3,4,5,6]"
+      :key="key"
+      class="col-3 g-0">
+      <folder-card/>
+    </div>
   </div>
 </template>
 
 <script setup>
 
+import FolderCard from '@/components/ulala/category/FolderCard.vue'
 </script>
 
 <style scoped>
