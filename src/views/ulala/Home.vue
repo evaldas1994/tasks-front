5f80f09f84201483ae657518a98b483b99f1ca8e
evald <evaldas.tuleikis@gmail.com>
2025-09-19 20:39:56 +0300

    Folderio kortelės komponentas

--- src/views/ulala/Home.vue
+++ src/views/ulala/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="row m-2">
     <div
-      v-for="(folder, key) in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]"
+      v-for="(folder, key) in [1,2,3,4,5,6,7,8,9,10]"
       :key="key"
       class="col-4 g-0">
       <folder-card/>
